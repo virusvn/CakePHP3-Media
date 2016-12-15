32a806bc42e58109ffe1e080f233d0eca3ce8c26
Nhan Nguyen <nhan_nguyen@brangista.com>
2016-12-15 18:55:48 +0900

    s-ceo

--- webroot/js/ckeditor/config.js
+++ webroot/js/ckeditor/config.js
@@ -2,13 +2,38 @@
  * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
+CKEDITOR.plugins.add( '5questions', {
+	icons: '5questions',
+	init: function( editor ) {
+		editor.addCommand( '5questions', {
+			exec: function( editor ) {
+				editor.insertHtml( '<div class="question5 middle mb-20"><img src="/images/common/5questions.png"></div>' );
+			}
+		});
+		editor.ui.addButton( '5questions', {
+			label: 'Insert 5 questions title',
+			command: '5questions',
+			toolbar: 'others'
+		});
+	}
+});
+CKEDITOR.stylesSet.add( 'my_styles', [
+	// Block-level styles.
+	{ name: '見出し', element: 'h2', attributes: { 'class': 'headline mb-20' },},
+	{ name: 'テキスト', element: 'p',  attributes: { 'class': 'text1 mb-20' }},
+	{ name: 'Mention',  element: 'p', attributes: { 'class': 'mention mb-20' } },
+	{ name: 'Q質問見出し', element: 'span' },
+	{ name: '質問見出し', element: 'h4', attributes: { 'class': 'headline_q mb-20' } },
+	{ name: '回答', element: 'p', attributes: { 'class': 'answer mb-20' } },
 
+
+]);
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
     
-    	config.extraPlugins = 'gallery';
+    	config.extraPlugins = 'gallery,colorbutton,5questions';
     	
     	config.language = 'ja';
     	
@@ -16,7 +41,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 		config.allowedContent = true;
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-    	/*	 
+
 	 config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
@@ -25,17 +50,18 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'forms' },
 		{ name: 'tools' },
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		 '/',
+		 {name : 'スーパCEO', groups: ['styles', 'others', 'colorbutton']},
+
 	];
-	 */
-	
-    	config.toolbar = [
+
+	config.contentsCss = '/css/article.css';
+	config.stylesSet = 'my_styles';
+/*
+	config.toolbar = [
 		{ name: 'styles', items: [ 'Format' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
 		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
@@ -47,11 +73,10 @@ CKEDITOR.editorConfig = function( config ) {
 		'/',
 	];
     	
-	 
+	 */
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
+	config.removeButtons = 'Underline,Subscript,Superscript,Image';
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
